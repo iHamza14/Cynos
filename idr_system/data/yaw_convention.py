0deb8a0573e9bf46e8106805fc4d61aa26296469
Harsh Dahiya <dahiyaharsh2005@gmail.com>
2026-09-18 10:17:53 +0530

    init

--- idr_system/data/yaw_convention.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def wrap_angle_rad(angle_rad):
-    """Wraps an angle in radians to [-pi, pi]."""
-    return (angle_rad + np.pi) % (2 * np.pi) - np.pi
--- idr_system/tests/test_dataset.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-def test_dataset_placeholder():
-    """Placeholder to verify test suite is functional."""
-    assert True
